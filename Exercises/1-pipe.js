5c560cf3271c452116b3bf505064647c6b5c4595
Danil Batishchev <yoloquence@gmail.com>
2021-02-09 20:21:56 +0200

    Completed Tasks

--- Exercises/1-pipe.js
+++ Exercises/1-pipe.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const pipe = (...fns) => x => null;
+const pipe = (...fns) => {
+  for (const fn of fns) {
+    if (typeof fn !== 'function') {
+      throw 'At least one input is not function type';
+    }
+  }
+  return x => fns.reduce((v, f) => f(v), x);
+};
 
 module.exports = { pipe };
--- Exercises/2-compose.js
+++ Exercises/2-compose.js
@@ -1,5 +1,27 @@
 'use strict';
 
-const compose = (...fns) => x => null;
+const compose = (...fns) => {
+  const handlers = [];
+  const composed = x => {
+    let res = x;
+    let i = fns.length - 1;
+    try {
+      for (i; i > -1; i--) {
+        res = fns[i](res);
+      }
+    } catch (err) {
+      for (const handler of handlers) {
+        handler(err);
+        return undefined;
+      }
+    }
+    return res;
+  };
+  composed.on = (type, handler) => {
+    if (type === 'error') handlers.push(handler);
+
+  };
+  return composed;
+};
 
 module.exports = { compose };
